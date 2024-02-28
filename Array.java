41dd06d88bc0c0c7b679c05e99b5b766584fd932
riya pandey <riyapandey97108@gmail.com>
2024-02-28 17:23:08 +0530

    add new file

---
+++ Array.java
@@ -0,0 +1,15 @@
+
+public class Array 
+{
+    public static void main(String[] args) 
+    {
+        int data[][]={{12,22,33},
+                {90,78,21},
+                {99,11,21}};
+    System.out.println(data[0][0]);
+    System.out.println(data [1][1]);
+    System.out.println(data[2][2]);
+            
+    }
+    
+}
